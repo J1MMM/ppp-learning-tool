68e513ce9b6edcec3e0f9f2f0aae871f252569f2
J1MMM <jimuelbaraero00@gmail.com>
2024-01-28 16:56:41 +0800

    loading-logo

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -3,7 +3,9 @@ import UseRefreshToken from '../hooks/useRefreshToken';
 import useAuth from '../hooks/useAuth';
 import { Outlet } from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+import pppLogo from '../assets/images/ppp-icon.png';
 
 const PersistLogin = () => {
     const [isLoading, setIsLoading] = useState(true)
@@ -35,7 +37,18 @@ const PersistLogin = () => {
         <>
             {isLoading ?
                 <Box sx={{ display: 'flex', width: '100%', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-                    <CircularProgress size={50} />
+                    <Box height={'100vh'} width={'100%'} display={'flex'} flexDirection={'column'} gap={3} justifyContent={'center'} alignItems={'center'}>
+                        <Box sx={{
+                            position: 'relative',
+                            width: '100%',
+                            height: '100%',
+                            maxHeight: 100,
+                            maxWidth: 100,
+                        }}>
+                            <img src={pppLogo} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                        </Box>
+                        <Typography variant='h6' color={'#2DA544'}>Loading please wait...</Typography>
+                    </Box>
                 </Box>
                 : <Outlet />
 
